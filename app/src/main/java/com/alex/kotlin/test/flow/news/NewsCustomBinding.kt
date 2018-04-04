cf3e84c8fb58c8ac2bb1745812ae20f4b241d49e
AlexandrKorzch <androidkorzchalexandr@gmail.com>
2018-04-04 11:07:34 +0300

    dataBinding null safety

--- app/src/main/java/com/alex/kotlin/test/flow/news/NewsCustomBinding.kt
+++ app/src/main/java/com/alex/kotlin/test/flow/news/NewsCustomBinding.kt
@@ -3,6 +3,7 @@ package com.alex.kotlin.test.flow.news
 import android.databinding.BindingAdapter
 import android.databinding.ObservableList
 import android.support.v7.widget.RecyclerView
+import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import com.alex.kotlin.test.adapter.ArticlesAdapter
@@ -24,10 +25,14 @@ fun setFormattedDate(textView: TextView, date: String) {
 }
 
 @BindingAdapter("bind:imageUrl")
-fun setImageUrl(imageView: ImageView, url: String) {
-    Picasso.with(imageView.context)
-            .load(url)
-            .into(imageView)
+fun setImageUrl(imageView: ImageView, url: String?) {
+    if(url != null){
+        Picasso.with(imageView.context)
+                .load(url)
+                .into(imageView)
+    }else{
+        imageView.visibility = View.GONE
+    }
 }
 
 @BindingAdapter("android:onRefresh")
